b19d9ae8e52fd65f9a09ff65c9822d2a1d59ddab
IrakliGiorgadze <ika.giorgadze@gmail.com>
2022-05-05 18:28:45 +0400

    fixing task02

--- task03-map.go
+++ task03-map.go
@@ -1,8 +1,23 @@
 package homework
 
+// func sortMapValues(input map[int]string) (result []string) {
+// 	for _, value := range input {
+// 		result = append(result, value)
+// 	}
+// 	return result
+// }
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
-	for _, value := range input {
-		result = append(result, value)
+	//return map values in sorted order using sort
+	var keys []int
+	for k := range input {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		result = append(result, input[k])
 	}
 	return result
+
 }
