d46189f59bbc3f741328b6205d17f26d2bf4e425
IrakliGiorgadze <ika.giorgadze@gmail.com>
2022-05-05 18:23:54 +0400

    fixing task02

--- task02-slice.go
+++ task02-slice.go
@@ -2,7 +2,7 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	for i := len(input) - 1; i >= 0; i-- {
-		result = append(result, input[i])
+		result = append(result, int64(input[i]))
 	}
 	return result
 }
--- task03-map.go
+++ task03-map.go
@@ -1,8 +1,8 @@
 package homework
 
 func sortMapValues(input map[int]string) (result []string) {
-	for key := range input {
-		result = append(result, input[key])
+	for _, value := range input {
+		result = append(result, value)
 	}
 	return result
 }
